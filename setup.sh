57cb9a2459a0605224f7555fa881f1d28d46c520
ArnaudGT <93054055+ArnaudGT@users.noreply.github.com>
2021-11-13 00:34:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ echo "-------------------"
 echo "C'est partit Frèro!"
 echo "-------------------"
 
+# // Pacman //
 PKGS=(
 'yay'
 'python-pip'
@@ -19,6 +20,7 @@ for PKG in "${PKGS[@]}"; do
     sudo pacman -S "$PKG" --noconfirm --needed
 done
 
+# // AUR //
 PKGS=(
 'heroic-games-launcher-bin'
 'nordic-darker-standard-buttons-theme'
@@ -26,16 +28,34 @@ PKGS=(
 'nordic-kde-git'
 'nordic-theme'
 'noto-fonts-emoji'
+'scrcpy'
 )
 
 for PKG in "${PKGS[@]}"; do
     yay -S --noconfirm $PKG
 done
 
+# // Konsave //
 pip install konsave
 konsave -i $HOME/Manjaro-PostInstall/pointfichiers.knsv
 sleep 1
 konsave -a pointfichiers
+
+# // Youtube-DL //
+pip install --upgrade youtube_dl
+
+
+# // Protonup //
+pip3 install protonup
+protonup
+
+# // XP-Pen Drivers //
+wget https://download01.xp-pen.com/file/2021/11/XP-PEN-pentablet-3.2.1.211019-1.x86_64.tar.gz
+tar -x -f /home/arnaud/XP-PEN-pentablet-3.2.1.211019-1.x86_64.tar.gz
+cd ${HOME}/xp-pen-pentablet-3.2.1.211019-1.x86_64/
+sh install.sh
+cd ~
+
 
 echo -e "\nTravail Terminé!\n"
 exit
